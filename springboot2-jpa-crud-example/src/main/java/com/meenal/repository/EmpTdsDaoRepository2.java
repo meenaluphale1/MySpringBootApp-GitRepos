c5de79bc8fcec736016eeb234337de5b26047bce
MeenalAnand <meenalanand@git.com>
2020-07-16 10:50:04 +0530

    Commit 1 - First Commit

---
+++ springboot2-jpa-crud-example/src/main/java/com/meenal/repository/EmpTdsDaoRepository2.java
@@ -0,0 +1,12 @@
+package com.meenal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.meenal.entity.EmpTds;
+
+
+public interface EmpTdsDaoRepository2  extends JpaRepository<EmpTds, Integer>{
+
+
+
+}
