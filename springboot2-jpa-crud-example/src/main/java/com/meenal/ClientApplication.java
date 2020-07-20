63506c9d412227db213b2b8971189a464c81d7ed
MeenalAnand <meenalanand@git.com>
2020-07-20 15:59:15 +0530

    testing rest controller

--- springboot2-jpa-crud-example/src/main/java/com/meenal/ClientApplication.java
+++ springboot2-jpa-crud-example/src/main/java/com/meenal/ClientApplication.java
@@ -12,9 +12,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ClientApplication   {
+public class ClientApplication   {	
+	
 	public static void main(String[] args) {		
 		SpringApplication.run(ClientApplication.class, args);
 	}	
